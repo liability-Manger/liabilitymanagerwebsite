ecaca130f33611dfaed903243bfe918d5e97c489
Alaaharmoush2003 <152899665+Alaaharmoush2003@users.noreply.github.com>
2024-02-14 19:51:11 +0200

    Update Dockerfile.php

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,11 +1,15 @@
 # Use the official PHP with Apache image
 FROM php:apache
 
-# Install dependencies
+# Set working directory
+WORKDIR /var/www/html
+
+# Update package manager and install dependencies
 RUN apt-get update && apt-get install -y \
     zlib1g-dev \
     libzip-dev \
-    && docker-php-ext-install mysqli pdo_mysql zip
+    && docker-php-ext-install mysqli pdo_mysql zip \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Set recommended PHP.ini settings for development
 RUN mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini"
@@ -24,5 +28,3 @@ EXPOSE 80
 
 # Start Apache
 CMD ["apache2-foreground"]
-
-
