8c67f8a6a22807c550f7c3b9dbccd4011450a807
dani kais <109339926+danikais@users.noreply.github.com>
2024-02-23 13:26:14 +0200

    Update Dockerfile.php

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,24 +1,13 @@
-# Use the official phpMyAdmin image as a base
+# Use an official phpMyAdmin image as base
 FROM phpmyadmin/phpmyadmin:latest
 
 # Set environment variables for MySQL connection
 ENV PMA_HOST=mysql
 ENV PMA_USER=admin
 ENV PMA_PASSWORD=admin
-ENV PMA_ARBITRARY=1
 
-# If you need to install additional tools or PHP extensions, you can do so here
-# For example, to install vim and a PHP extension:
-# RUN apt-get update && apt-get install -y vim \
-#     && docker-php-ext-install mysqli
-
-# Copy a custom configuration file if needed
-# COPY config.user.inc.php /etc/phpmyadmin/config.user.inc.php
-
-# Expose port 80 to access phpMyAdmin
+# The port phpMyAdmin will run on
 EXPOSE 80
 
-# The official phpMyAdmin image uses an entrypoint script that automatically starts Apache
-# If you need to execute additional commands, you can override the entrypoint or cmd directives
-# ENTRYPOINT [""]
-# CMD ["apache2-foreground"]
+# Start phpMyAdmin
+CMD ["apache2-foreground"]
